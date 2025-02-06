85fce84125b1264d433b67bc42f66d59544d9ef9
Harish Kumar <harishstudio877@gmail.com>
2025-02-06 07:47:05 +0530

    login for added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,7 @@
 
 const Header = () => {
 
-    const BG_URL = "https://assets.nflxext.com/ffe/siteui/vlv3/fb5cb900-0cb6-4728-beb5-579b9af98fdd/web/IN-en-20250127-TRIFECTA-perspective_cf66f5a3-d894-4185-9106-5f45502fc387_medium.jpg"
-
     const LOGO_URL = 'https://help.nflxext.com/helpcenter/OneTrust/oneTrust_production/consent/87b6a5c0-0104-4e96-a291-092c11350111/01938dc4-59b3-7bbc-b635-c4131030e85f/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png'
-
-
 
     return (
         <div className="absolute px-8 py-2 bg-gradient-to-b from-black z-10 ">
