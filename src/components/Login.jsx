4ee7eb773b7bc2f1bf0f0671e653d90e77fc9f57
Harish Kumar <harishstudio877@gmail.com>
2025-02-08 09:10:04 +0530

    input validation

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -3,6 +3,7 @@
 import { useState } from "react"
 import Header from "./Header"
 import { useRef } from "react"
+import { checkValidData } from "../utils/validate"
 
 const Login = () => {
     const BG_URL = "https://assets.nflxext.com/ffe/siteui/vlv3/fb5cb900-0cb6-4728-beb5-579b9af98fdd/web/IN-en-20250127-TRIFECTA-perspective_cf66f5a3-d894-4185-9106-5f45502fc387_medium.jpg"
@@ -18,11 +19,9 @@ const Login = () => {
     }
 
     const handleSubmit = () => {
-        const [email, password, name] = [email.current.value, password.current.value, name.current.value]
-
-        if (!email || !password) {
-            setError("Please enter email and password")
-            return
+        const message = checkValidData(email.current.value, password.current.value)
+        if (message) {
+            setError(message)
         }
 
         if (isLoginForm) {
