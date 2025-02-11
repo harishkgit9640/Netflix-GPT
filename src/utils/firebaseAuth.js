5727305c7123c1e4e7d7040ee177f7d5f6aa7a8a
Harish Kumar <harishstudio877@gmail.com>
2025-02-11 07:45:59 +0530

    firebase login and deploy setup

---
+++ src/utils/firebaseAuth.js
@@ -0,0 +1,24 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getAuth } from "firebase/auth";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyAzppQKQ1Z8Z0aPcXriXQ0SamheNFzCMs0",
+    authDomain: "netflixgpt-42748.firebaseapp.com",
+    projectId: "netflixgpt-42748",
+    storageBucket: "netflixgpt-42748.firebasestorage.app",
+    messagingSenderId: "757315219473",
+    appId: "1:757315219473:web:feef3e8e4dc398283a81b0",
+    measurementId: "G-6M5SJ5YZX5"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+export const auth = getAuth();
