6bc9b4fd93d0f8f48c2c390e62930792f7707389
Harish Kumar <harishstudio877@gmail.com>
2025-02-14 07:01:35 +0530

    TMDB api configured

---
+++ src/utils/constants.js
@@ -0,0 +1,22 @@
+export const LOGO_URL = 'https://help.nflxext.com/helpcenter/OneTrust/oneTrust_production/consent/87b6a5c0-0104-4e96-a291-092c11350111/01938dc4-59b3-7bbc-b635-c4131030e85f/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png'
+
+
+export const USER_AVATAR = "https://e7.pngegg.com/pngimages/799/987/png-clipart-computer-icons-avatar-icon-design-avatar-heroes-computer-wallpaper-thumbnail.png"
+
+
+export const BG_URL = "https://assets.nflxext.com/ffe/siteui/vlv3/fb5cb900-0cb6-4728-beb5-579b9af98fdd/web/IN-en-20250127-TRIFECTA-perspective_cf66f5a3-d894-4185-9106-5f45502fc387_medium.jpg"
+
+
+// TMDB API CREDENTIALS
+
+export const API_ACCESS_TOKEN = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmNjI0NzE5M2JkYjVmMjYzZTliNDE2MzQxOTQzNWM3NSIsIm5iZiI6MTczOTQxNjY1MC42MTUsInN1YiI6IjY3YWQ2NDRhNjQ1NjY5MTM3ZTlmNDk1MiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.ysx7vVMJyYDFBymcFik2GM-_rt6Ea8mu4JCLTAjAhF0"
+export const API_KEY = "f6247193bdb5f263e9b4163419435c75"
+
+
+export const API_OPTIONS = {
+    method: 'GET',
+    headers: {
+        accept: 'application/json',
+        Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmNjI0NzE5M2JkYjVmMjYzZTliNDE2MzQxOTQzNWM3NSIsIm5iZiI6MTczOTQxNjY1MC42MTUsInN1YiI6IjY3YWQ2NDRhNjQ1NjY5MTM3ZTlmNDk1MiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.ysx7vVMJyYDFBymcFik2GM-_rt6Ea8mu4JCLTAjAhF0'
+    }
+};
