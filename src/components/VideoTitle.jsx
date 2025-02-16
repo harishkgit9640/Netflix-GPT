2c1426c04b0bf54bf25bc0705248e7a9c346d6d5
Harish Kumar <harishstudio877@gmail.com>
2025-02-16 10:06:06 +0530

    background video completed

--- src/components/VideoTitle.jsx
+++ src/components/VideoTitle.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 const VideoTitle = ({ title, overview }) => {
     return (
         <>
-            <div className='absolute top-52 left-10 text-black'>
+            <div className='w-screen aspect-video absolute pt-[15%] px-6 md:px-12 bg-gradient-to-r from-black text-white'>
                 <h1 className='text-6xl py-4 font-bold'>{title}</h1>
                 <p className='w-1/3 text-sm'>{overview}</p>
                 <div className="action-btn mt-5">
@@ -12,7 +12,6 @@ const VideoTitle = ({ title, overview }) => {
                     <button className='px-6 py-2 mx-2 bg-red-600 text-white rounded-md' > More Info </button>
                 </div>
             </div>
-            {/* <BackgroundVideo /> */}
         </>
     )
 }
